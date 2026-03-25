fbd6160110c85763fcfdb5e3705d0975236c88bd
titusbspgit <52008376+titusbspgit@users.noreply.github.com>
2026-03-25 16:32:19 +0530

    Add GPIO testcase test_gpio_pedge_all_pads_en autogenerated code

---
+++ mpsoc_output/output/gpio_1530_25032026/test_gpio_pedge_all_pads_en/test_define.c
@@ -0,0 +1,8 @@
+#include <lss_sysreg.h>
+#include <stdio.h>
+/* Note: helper listed "test_define.c"; self-include omitted to avoid recursion */
+#include <test_common.h>
+#include <gpio/gpio_def.h>
+#include <gpio/gpio_offset.h>
+
+#define CNT 49
