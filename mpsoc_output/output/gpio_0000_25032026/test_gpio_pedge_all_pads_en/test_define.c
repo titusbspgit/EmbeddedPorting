387c3ad2fe5a6aa15d69ba429979aeb54f0955ca
titusbspgit <52008376+titusbspgit@users.noreply.github.com>
2026-03-25 15:41:22 +0530

    Add GPIO testcase test_gpio_pedge_all_pads_en autogenerated code

---
+++ mpsoc_output/output/gpio_0000_25032026/test_gpio_pedge_all_pads_en/test_define.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <lss_sysreg.h>
+#include <test_common.h>
+#include <gpio/gpio_def.h>
+#include <gpio/gpio_offset.h>
+
+#define GPIO0 1
+/* Optional debug prints guarded by DEBUG_DISPLAY */
+/* #define DEBUG_DISPLAY 1 */
+
+/* No skip registers provided by helper for this testcase */
