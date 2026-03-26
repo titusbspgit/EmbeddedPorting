04259eb3bd8baf6b8ddbd7739d5ac8a9681e170d
titusbspgit <52008376+titusbspgit@users.noreply.github.com>
2026-03-26 17:18:09 +0530

    Add GPIO testcase test_gpio_negedge_intr_en autogenerated code (test_define.c)

---
+++ mpsoc_output/output/GPIO_0000_26032026/test_gpio_negedge_intr_en/test_define.c
@@ -0,0 +1,8 @@
+#include <gpio/gpio_def.h>
+#include <gpio/gpio_offset.h>
+#include <lss_sysreg.h>
+
+/* Optional helper: base GPIO0 per-pin config register list starting at MIZAR_GPIO_GP0_GPIO_8 */
+static const unsigned long int gpio0_pin_cfg_base = MIZAR_GPIO_GP0_GPIO_8;
+
+/* IO control group macros are provided by gpio_def/offset */
